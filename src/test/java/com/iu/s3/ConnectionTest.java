c984179bf84db525bde9477c380d519f828f6862
llddoo <74532161+llddoo@users.noreply.github.com>
2021-03-22 17:07:31 +0900

    03.22 DB jdbc연결

---
+++ src/test/java/com/iu/s3/ConnectionTest.java
@@ -0,0 +1,21 @@
+package com.iu.s3;
+
+import static org.junit.Assert.*;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ConnectionTest extends MyAbstractTest {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Test
+	public void connectionTest() throws Exception{
+		assertNotNull(dataSource.getConnection());
+	}
+
+
+}
